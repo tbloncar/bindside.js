88dbf1a0140c81a303300d5afe60491cfd98e242
Travis Loncar <loncar.travis@gmail.com>
2015-05-17 15:04:15 -0400

    Retab.

--- src/bindside.js
+++ src/bindside.js
@@ -10,76 +10,76 @@
  */
 
 var bindside = (function __bindside__() {
-	/**
-	 * Represents a generic value.
-	 * @class
-	 */
-	function Value(value) {
-		this.value = value;	
-	}
+  /**
+   * Represents a generic value.
+   * @class
+   */
+  function Value(value) {
+    this.value = value; 
+  }
 
-	Value.prototype.get = function() {
-		return this.value;	
-	};
+  Value.prototype.get = function() {
+    return this.value;  
+  };
 
-	/**
-	 * Proxy object used to track computed 
-	 * property dependencies.
-	 * @class
-	 *
-	 * @param {ViewModel} vm - The bindside view model
-	 */
-	function ViewModelProxy(vm) {
-		var self = this;
+  /**
+   * Proxy object used to track computed 
+   * property dependencies.
+   * @class
+   *
+   * @param {ViewModel} vm - The bindside view model
+   */
+  function ViewModelProxy(vm) {
+    var self = this;
 
-		this.vm = vm;	
-		this.propsUsed = [];
+    this.vm = vm; 
+    this.propsUsed = [];
 
-		// Mime VM props, injecting dependency tracking
-		// into each property function
-		for(var p in this.vm.props) {
+    // Mime VM props, injecting dependency tracking
+    // into each property function
+    for(var p in this.vm.props) {
       /* jshint ignore:start */
-			(function(p) {
-				self[p] = function() {
-					self.propsUsed.push(p);
-					return self.vm[p]();
-				};
-			})(p);
+      (function(p) {
+        self[p] = function() {
+          self.propsUsed.push(p);
+          return self.vm[p]();
+        };
+      })(p);
       /* jshint ignore:end */
-		}
-	}
+    }
+  }
 
-	/**
-	 * The bindside view model; used to declare data-bound
-	 * properties and actions.
-	 * @class
-	 *
-	 * @param {string} selector - A DOM selector used to limit the VM scope
-	 */
+  /**
+   * The bindside view model; used to declare data-bound
+   * properties and actions.
+   * @class
+   *
+   * @param {string} selector - A DOM selector used to limit the VM scope
+   */
   function ViewModel(selector) {
-		var _selector = selector || 'body';
+    var _selector = selector || 'body';
 
     this.el = document.querySelector(_selector); 
     this.props = {};
-		this.propMap = {};
+    this.propMap = {};
     this.actions = {};
   }
 
   ViewModel.prototype.prop = function(name, cb) {
-		var self = this, vmProxy;
+    var self = this, vmProxy;
 
-		// Callback present -> computed property
+    // Callback present -> computed property
     if(cb) {
       this.props[name] = function() {
-				return cb.call(self);
+        return cb.call(self);
       }; 
     } else {
       this.props[name] = null; 
     }
 
-		// Create VM-level property function
+    // Create VM-level property function
     this[name] = function(value) {
-			var val = value === undefined ? undefined : new Value(value);
+      var val = value === undefined ? undefined : new Value(value);
 
       if(cb) {
         if(val) {
@@ -90,36 +90,36 @@ var bindside = (function __bindside__() {
       } else {
         if(val) {
           this.props[name] = val.get(); 
-					assignValueToNodes(this.getNodesForProp(name), val.get());
+          assignValueToNodes(this.getNodesForProp(name), val.get());
 
-					if(this.propMap[name]) {
-						this.propMap[name].forEach(function(p) {
-							assignValueToNodes(self.getNodesForProp(p), self[p]());
-						});
-					}
+          if(this.propMap[name]) {
+            this.propMap[name].forEach(function(p) {
+              assignValueToNodes(self.getNodesForProp(p), self[p]());
+            });
+          }
         } else {
           return this.props[name] || '';
         }
       }
     };
 
-		// If property is computed, invoke the callback with
-		// a VM proxy object to track property dependencies
-		if(cb) {
-			vmProxy = new ViewModelProxy(self);
-			cb.call(vmProxy); 
+    // If property is computed, invoke the callback with
+    // a VM proxy object to track property dependencies
+    if(cb) {
+      vmProxy = new ViewModelProxy(self);
+      cb.call(vmProxy); 
 
-			// For each VM property used in the property
-			// computation, push the computed property name
-			// into its collection of dependent computed properties
-			for(var i = 0, l = vmProxy.propsUsed.length; i < l; i++) {
-				if(!self.propMap[vmProxy.propsUsed[i]]) {
-					self.propMap[vmProxy.propsUsed[i]] = [];
-				}
+      // For each VM property used in the property
+      // computation, push the computed property name
+      // into its collection of dependent computed properties
+      for(var i = 0, l = vmProxy.propsUsed.length; i < l; i++) {
+        if(!self.propMap[vmProxy.propsUsed[i]]) {
+          self.propMap[vmProxy.propsUsed[i]] = [];
+        }
 
-				self.propMap[vmProxy.propsUsed[i]].push(name);
-			}
-		}
+        self.propMap[vmProxy.propsUsed[i]].push(name);
+      }
+    }
   };
 
   ViewModel.prototype.action = function(name, cb) {
@@ -130,21 +130,21 @@ var bindside = (function __bindside__() {
     };
   };
 
-	ViewModel.prototype.model = function(model) {
-		this.model = model;
+  ViewModel.prototype.model = function(model) {
+    this.model = model;
 
-		if(model) {
-			for(var k in model) {
-				this[k](model[k]);		
-			}	
-		} else {
-			return this.model;	
-		}
-	};
+    if(model) {
+      for(var k in model) {
+        this[k](model[k]);    
+      } 
+    } else {
+      return this.model;  
+    }
+  };
 
-	ViewModel.prototype.getNodesForProp = function(prop) {
-		return this.el.querySelectorAll('[data-prop="' + prop + '"]');
-	};
+  ViewModel.prototype.getNodesForProp = function(prop) {
+    return this.el.querySelectorAll('[data-prop="' + prop + '"]');
+  };
 
   ViewModel.prototype.bindProps = function() {
     var self = this;
@@ -161,26 +161,26 @@ var bindside = (function __bindside__() {
 
       /* jshint ignore:start */
       (function(p) {
-				for(var i = 0, l = nodes.length; i < l; i++) {
-					nodes[i].addEventListener('change', bindProp); 
-				}
+        for(var i = 0, l = nodes.length; i < l; i++) {
+          nodes[i].addEventListener('change', bindProp); 
+        }
       })(p);
       /* jshint ignore:end */
     }
   };
 
-	function assignValueToNodes(nodes, value) {
-		for(var i = 0, l = nodes.length; i < l; i++) {
-			node = nodes[i];
+  function assignValueToNodes(nodes, value) {
+    for(var i = 0, l = nodes.length; i < l; i++) {
+      node = nodes[i];
 
-			if(node instanceof HTMLInputElement ||
-				 node instanceof HTMLTextAreaElement) {
-				node.value = value;			
-			}	else {
-				node.innerHTML = value;	
-			}
-		}
-	}
+      if(node instanceof HTMLInputElement ||
+         node instanceof HTMLTextAreaElement) {
+        node.value = value;     
+      } else {
+        node.innerHTML = value; 
+      }
+    }
+  }
 
   /**
    * Creates the view model
@@ -197,8 +197,8 @@ var bindside = (function __bindside__() {
     } else {
       document.addEventListener('DOMContentLoaded', vm.bindProps.bind(vm));
     }
-
-		return vm;
+    
+    return vm;
   }
 
   return {
